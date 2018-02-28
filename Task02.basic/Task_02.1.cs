be43dd523a39cddc4e402d49f3bde79236933150
KosovoJeSrbija <egor.lebedev1337@gmail.com>
2018-02-28 11:01:07 +0300

    task2(1,2)

---
+++ Task02.basic/Task_02.1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert a ");
+            string valuea = Console.ReadLine();
+            int a = int.Parse(valuea);
+
+            Console.Write("Insert b ");
+            string valueb = Console.ReadLine();
+            int b = int.Parse(valueb);
+            int c;
+            if (a <= 0 || b <= 0)
+            {
+                Console.WriteLine("Incorrect value");
+            }
+            else
+                Console.WriteLine(c = a * b);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Task02.basic/task_02.2/task_02.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_02._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N=");
+            string value = Console.ReadLine();
+            int n = int.Parse(value);
+            int i;
+
+            for (i = 0; i <= n; i++);
+            Console.Write("*", i);
+
+
+        }
+    }
+}
